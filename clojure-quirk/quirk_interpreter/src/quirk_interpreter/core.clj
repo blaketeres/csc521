c0c0ce25f4eb1d554763cdee574d07727bb51ca7
Blake Teres <blaketeres@gmail.com>
2017-04-17 20:52:17 -0400

    add instaparse functionality with auto whitespace

--- clojure-quirk/quirk_interpreter/src/quirk_interpreter/core.clj
+++ clojure-quirk/quirk_interpreter/src/quirk_interpreter/core.clj
@@ -1,7 +1,6 @@
 (ns example.core
   (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def quirk
+  (insta/parser
+    (clojure.java.io/resource "quirkGrammar.ebnf") :auto-whitespace :standard))
